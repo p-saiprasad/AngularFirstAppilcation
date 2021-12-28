35a490dd5d795f16907eedf884a0b3c9a38527e2
p-saiprasad <saiprasadp@outlook.com>
2021-12-28 08:27:37 +0530

    2 files modified, 3 added, 1 deleted

---
+++ src/app/Netflix/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app1',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {}
